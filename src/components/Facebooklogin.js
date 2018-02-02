ca1f12ddee5d0d0f306c71ddf5992634fd9377f4
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-02-02 16:19:41 -0500

    Initial commit

--- src/components/Facebooklogin.js
+++ src/components/Facebooklogin.js
@@ -75,12 +75,9 @@ class Facebooklogin extends Component{
     render(){
         return(
              //<img src={facebook} title="facebook login" alt="facebook" onclick={ () => this.facebooklogin() } />
-            // <button onClick={() => this.facebooklogin()}/>
+            <button onClick={() => this.facebooklogin()}/>
             //<div class="fb-login-button" data-size="medium" data-auto-logout-link="true" onClick={ () => this.facebooklogin() }></div>
             // data-onlogin="checkLoginState();"
-            <div class="fb-login-button">
-                <button onClick={() => this.facebooklogin()}/>
-            </div>
         )
     }
 
