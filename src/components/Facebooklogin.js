56479ed7ce7af0bc254046d0ef0549734309941d
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-02-03 14:38:00 -0500

    Added styles

--- src/components/Facebooklogin.js
+++ src/components/Facebooklogin.js
@@ -85,7 +85,7 @@ class Facebooklogin extends Component{
                     PIXELS
                 </div>
                 <div className="fblogin">
-                    <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" onclick={ () => this.facebooklogin() }></div>
+                    <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" onClick={ () => this.facebooklogin() }></div>
                 </div>            
             </div>
         </div>
