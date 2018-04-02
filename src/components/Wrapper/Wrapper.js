113b433b2364dce171894bcdaaa7043d7527d57a
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-02 16:46:58 -0400

    Changes made

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import FacebookLogin from 'react-facebook-login';
-import Facebooklogin from './components/LoginPage/Facebooklogin';
-import IndexPage from './components/IndexPage';
+import Facebooklogin from '../LoginPage/Facebooklogin';
+import IndexPage from '../IndexPage';
 // import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from './C:/Users/Mano/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/react-router-dom';
+
 
 // const responseFacebook = (response) => {
 //     console.log(response);
@@ -16,14 +18,3 @@ export const Wrapper = () => (
         <IndexPage />        
     </BrowserRouter>
 );
-
-ReactDOM.render(
-    // <FacebookLogin
-    // appId="1099422136861554"
-    // autoLoad={true}
-    // fields="name,email,picture"
-    // //onClick={componentClicked}
-    // callback={responseFacebook} />
-    <Wrapper />    
-    , document.getElementById('root'));
-registerServiceWorker();
