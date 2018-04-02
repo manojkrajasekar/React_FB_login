4b07afc1ce3c5bd4544b49066a98ab21ca6cdd0f
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-02 17:30:06 -0400

    Changes made

--- src/components/LoginPage/Facebooklogin.js
+++ src/components/LoginPage/Facebooklogin.js
@@ -106,7 +106,7 @@ class Facebooklogin extends Component{
       }
 
       GoToLandingPage = () => {  
-          if(this.state.isLoggedIn()) {
+          if(this.state.isLoggedIn) {
             return <Redirect to ="/LandingPage"/>;
           }
          //this.props.history.push('/LandingPage');
