3fa1368892595e71fb8e15bbd5b5cce39ad9b2b6
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-02 12:36:18 -0400

    Updated with changes

--- src/components/Facebooklogin.js
+++ src/components/Facebooklogin.js
@@ -67,6 +67,10 @@ class Facebooklogin extends Component{
         });
     }
 
+    loginAlert = () => {
+        console.log('Logging in');
+    }
+
     render(){
         return(
              //<img src={facebook} title="facebook login" alt="facebook" onclick={ () => this.facebooklogin() } />
@@ -77,7 +81,7 @@ class Facebooklogin extends Component{
                     <div className="app-title">
                         PIXELS
                     </div>
-                    <div className="fblogin">
+                    <div className="fblogin" onClick={this.loginAlert}>
                         <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" onClick={ () => this.facebooklogin() }></div>
                     </div>    
                 </div>
