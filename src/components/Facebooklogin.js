4ee72ae68dcd486d8c5ea96290607630422be13c
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-02 15:02:03 -0400

    Changes made

--- src/components/Facebooklogin.js
+++ src/components/Facebooklogin.js
@@ -5,26 +5,25 @@ import './Facebooklogin.css';
 
 class Facebooklogin extends Component{
     
-    componentDidMount(){
-        // Load the required SDK asynchronously for facebook, google and linkedin
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            //js.src = "//connect.facebook.net/en_US/sdk.js";
-            js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
-            //js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1099422136861554";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-        
-        window.fbAsyncInit = function() {
-            window.FB.init({
-              appId      : '1099422136861554',
-              cookie     : true,  // enable cookies to allow the server to access the session
-              xfbml      : true,  // parse social plugins on this page
-              version    : 'v2.8' // use version 2.1
-            });
-        };
+    componentDidMount() {
+            (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    //js.src = "//connect.facebook.net/en_US/sdk.js";
+                    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
+                    //js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1099422136861554";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+                
+                window.fbAsyncInit = function() {
+                    window.FB.init({
+                    appId      : '1099422136861554',
+                    cookie     : true,  // enable cookies to allow the server to access the session
+                    xfbml      : true,  // parse social plugins on this page
+                    version    : 'v2.8' // use version 2.1
+                    });
+                };
     }
     
     facebooklogin = () => {
@@ -39,25 +38,20 @@ class Facebooklogin extends Component{
             }.bind(this),{ scope : 'email,user_work_history,user_education_history,user_location,public_profile' });
     }
     
-    checkLoginState() {
-        alert("Checking Login Status")
+    checkLoginState = () => {
         console.log( "Checking login status..........." );
-        
         window.FB.getLoginStatus(function(response) {
-            alert("FB Callback")
-            console.log("----------->")
-            console.log(response)
             this.statusChangeCallback(response);
         }.bind(this));
     }
+
     
-    statusChangeCallback(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
+    statusChangeCallback = (response) => {
         if (response.status === 'connected') {
+            console.log('connected Fine');
             alert( "Connected to facebook. Retriving user from fb" );
             // Logged into your app and Facebook.
-            this.fetchDataFacebook();
+            this.testAPI();
         } else if (response.status === 'not_authorized') {
             console.log('Import error', 'Authorize app to import data', 'error')
         } else {
@@ -65,7 +59,7 @@ class Facebooklogin extends Component{
         }
     }
     
-    fetchDataFacebook = () => {
+    testAPI = () => {
         console.log('Welcome!  Fetching your information.... ');
 
         window.FB.api('/me', function(user) {
@@ -75,15 +69,7 @@ class Facebooklogin extends Component{
         });
     }
 
-    loginAuthenticate = () => {
-        if (response.status === 'connected') {
-            console.log('connected');
-        } 
-        else if (response.status === 'not_authorized') {
-            console.log('Import error', 'Authorize app to import data', 'error')
-        }
-    }
-
+    
     render(){
         return(
              //<img src={facebook} title="facebook login" alt="facebook" onclick={ () => this.facebooklogin() } />
@@ -97,7 +83,7 @@ class Facebooklogin extends Component{
                     <div className="fblogin">
                         <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" onClick={ () => this.facebooklogin() }></div>
                     </div>
-                    <div onClick={this.loginAuthenticate}><input type="submit">Cick Here</input></div>            
+                    <div><input type="submit">Cick Here</input></div>            
                 </div>
             </div>
          )
