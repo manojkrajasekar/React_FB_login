63633c10658078570b4d309e80ddf96f47f2803a
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-02 20:07:46 -0400

    Changes made

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,24 +3,24 @@ import { Redirect } from 'react-router-dom';
 
 class LandingPage extends Component {
 
-    componentDidMount() {
-            window.fbAsyncInit = function() {
-                window.FB.init({
-                appId      : '1099422136861554',
-                cookie     : true,  // enable cookies to allow the server to access the session
-                xfbml      : true,  // parse social plugins on this page
-                version    : 'v2.8' // use version 2.1
-                });
-        }
-    }
+    // componentDidMount() {
+    //         window.fbAsyncInit = function() {
+    //             window.FB.init({
+    //             appId      : '1099422136861554',
+    //             cookie     : true,  // enable cookies to allow the server to access the session
+    //             xfbml      : true,  // parse social plugins on this page
+    //             version    : 'v2.8' // use version 2.1
+    //             });
+    //     }
+    // }
 
     logout = () => {
         window.FB.getLoginStatus(function(response) {
             if (response.status === 'connected') {
                 console.log('Going to log out');
-                window.FB.logout((response) =>{
-                    console.log('logged out');
-                    return (<Redirect to ="/" />);
+                window.FB.logout((response) => {
+                    
+                    return (console.log('logged out'));
                 });
             }
         });
